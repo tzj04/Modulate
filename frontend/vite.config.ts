9ec1bf7a654be90012a8de8ed1bfbf8b15fcb01b
tzj04 <190485478+tzj04@users.noreply.github.com>
2026-01-18 15:20:48 +0800

    typo in filename

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
