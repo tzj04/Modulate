4f0d7cf07d546d3dff6cfd9286f67b51e93c98fc
tzj04 <190485478+tzj04@users.noreply.github.com>
2026-01-25 15:48:20 +0800

    hosting db

--- nusmods_data_processing/load.py
+++ nusmods_data_processing/load.py
@@ -3,20 +3,23 @@ from psycopg2.extras import execute_batch
 import os, json, dotenv
 
 dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
-DB_NAME = os.getenv("POSTGRES_DB")
-USER = os.getenv("POSTGRES_USER")
-PASSWORD = os.getenv("POSTGRES_PASSWORD")
+# DB_NAME = os.getenv("POSTGRES_DB")
+# USER = os.getenv("POSTGRES_USER")
+# PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 with open("filtered_modules.json") as f:
     modules = json.load(f)
 
-conn = psycopg2.connect(
-    host="localhost",
-    port=5432,
-    dbname= DB_NAME,
-    user=USER,
-    password=PASSWORD,
-)
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+# conn = psycopg2.connect(
+#     host="localhost",
+#     port=5432,
+#     dbname= DB_NAME,
+#     user=USER,
+#     password=PASSWORD,
+# )
 
 insert_sql = """
 INSERT INTO modules (code, title, description, faculty)
